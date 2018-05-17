3ee30c37f9d72856eee9b2a78ba6937c885fe95f
Kelvin Ferreira Souza <kelvin@ferreirasouza.com>
2018-05-17 10:46:52 -0300

    Alterado a funcionalidade de cadastro de fornecedor para uma modal

--- resources/views/fornecedores/listar.blade.php
+++ resources/views/fornecedores/listar.blade.php
@@ -31,7 +31,104 @@
                             <h5>Lista de Fornecedores</h5>
                             <span>Listagem dos fornecedores cadastrados</span>   
                         </div>
-                        <a href="{{route('cadastrarFornecedor')}}"><button type="button" class="btn btn-primary waves-effect waves-light btnCadUser"><i class="fa fa-user-plus"></i>Cadastrar Fornecedor</button></a>
+                        <!-- BOTAO CADASTRAR FORMA PAGAMENTO MODAL -->
+                        @foreach($fornecedores as $fornecedor)
+                        @if ($loop->first)
+                        <a href="" data-toggle="modal" data-target="#modalCadastrar{{$fornecedor->id}}" data-whatever="{{$fornecedor->id}}" data-whateverrazaosocial="{{$fornecedor->razaoSocial}}" data-whateverfantasia="{{$fornecedor->nomeFantasia}}" data-whatevercpfcnpj="{{$fornecedor->cpfCnpj}}" data-whateverierg="{{$fornecedor->ieRg}}" data-whateveremail="{{$fornecedor->email}}" data-whatevercep="{{$fornecedor->cep}}" data-whateverlagradouro="{{$fornecedor->lagradouro}}" data-whatevernumero="{{$fornecedor->numero}}" data-whateverbairro="{{$fornecedor->bairro}}" data-whateverestado="{{$fornecedor->estado}}" data-whatevermunicipio="{{$fornecedor->municipio}}" data-whateverfone="{{$fornecedor->fone}}"data-whateverativo="{{$fornecedor->isAtivo}}"><button type="button" class="btn btn-primary waves-effect waves-light btnCadUser"><i class="fa fa-user-plus"></i>Cadastrar Fornecedor</button></a>
+
+                        <!-- MODAL DE CADASTRAR -->
+                        <div class="modal fade" id="modalCadastrar{{$fornecedor->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                            <div class="modal-dialog modal-lg modalFornec" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header" style="background-color: #0cb6734 !important; color: white">
+                                        <h5 class="modal-title" id="exampleModalLongTitle" style="color: #fff">FORNECEDORES<i class="fa fa-help"></i></h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true" style="color: #fff">×</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form method="post" action="{{route ('salvarFornecedor')}}" class="formEditUser">
+                                            {{ csrf_field() }}
+                                            <div class="card-header">
+                                                <CENTER><h5>Cadastrar Fornecedor</h5></CENTER>
+                                            </div>
+                                            <div class="card-block">
+                                                <div class="form-group row">
+                                                    <div class="col-sm-6">
+                                                        <label for="razaoSocial" class="control-label labelInputEditUser">Razão Social:</label>
+                                                        <input type="text" class="form-control" name="razaoSocial" placeholder="Digite a Razão Social" required>
+                                                    </div>
+                                                    <div class="col-sm-6">
+                                                        <label for="nomeFantasia" class="control-label labelInputEditUser">Nome Fantasia:</label>
+                                                        <input type="text" class="form-control" name="nomeFantasia" placeholder="Digite a Nome Fantasia" required>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <div class="col-sm-2">
+                                                        <label for="cpfCnpj" class="control-label labelInputEditUser">CNPJ:</label>
+                                                        <input type="text" class="form-control" name="cpfCnpj" placeholder="Digite o CNPJ" required>
+                                                    </div>
+                                                    <div class="col-sm-2">
+                                                        <label for="ieRg" class="control-label labelInputEditUser">I.E:</label>
+                                                        <input type="text" class="form-control" name="ieRg" placeholder="Inscrição Estadual" required>
+                                                    </div>
+                                                    <div class="col-sm-6">
+                                                        <label for="email" class="control-label labelInputEditUser">E-mail:</label>
+                                                        <input type="text" class="form-control" name="email" placeholder="Digite a email" required>
+                                                    </div>
+                                                    <div class="col-sm-2">
+                                                        <label for="cep" class="control-label labelInputEditUser">CEP:</label>
+                                                        <input type="text" class="form-control" name="cep" placeholder="Digite a CEP" required>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <div class="col-sm-6">
+                                                        <label for="lagradouro" class="control-label labelInputEditUser">Lagradouro:</label>
+                                                        <input type="text" class="form-control" name="lagradouro" placeholder="Digite o Lagradouro" required>
+                                                    </div>
+                                                    <div class="col-sm-2">
+                                                        <label for="numero" class="control-label labelInputEditUser">Número:</label>
+                                                        <input type="text" class="form-control" name="numero" placeholder="Número" required>
+                                                    </div>
+                                                    <div class="col-sm-4">
+                                                        <label for="bairro" class="control-label labelInputEditUser">Bairro:</label>
+                                                        <input type="text" class="form-control" name="bairro" placeholder="Digite o bairro" required>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <div class="col-sm-4">
+                                                        <label for="municipio" class="control-label labelInputEditUser">Cidade:</label>
+                                                        <input type="text" class="form-control" name="municipio" placeholder="Digite o Cidade" required>
+                                                    </div>
+                                                    <div class="col-sm-4">
+                                                        <label for="estado" class="control-label labelInputEditUser">Estado:</label>
+                                                        <input type="text" class="form-control" name="estado" placeholder="Digite o Estado" required>
+                                                    </div>
+                                                    <div class="col-sm-2">
+                                                        <label for="fone" class="control-label labelInputEditUser">Fone:</label>
+                                                        <input type="text" class="form-control" name="fone" placeholder="Digite o Telefone" required>
+                                                    </div>
+                                                    <div class="col-sm-2">
+                                                        <label for="isAtivo" class="control-label labelInputEditUser">Status:</label>
+                                                        <select class="form-control labelInputEditUser" name="isAtivo">
+                                                            <option value="1">Ativo</option>
+                                                            <option value="0">Inativo</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>    
+                                            <div class="modal-footer modal-footer-formpag">
+                                                <button type="submit" class="btn btn-primary"><i class="icofont icofont-save"></i>Salvar</button>
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                            </div>       
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endif
+                        @endforeach
+                        <!-- FIM MODAL CADASTRO -->
                     </div>
                     <div class="card-block">
                         <div class="row">
--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,7 @@ Route::middleware(['auth'])->group(function () {
 
     /* Rotas Protegidas de Usuários */
     Route::get('/usuarios', 'UsuarioController@listarUsuarios')->name('listarUsuarios');
-    Route::get('/usuarios/editar/{id}', 'UsuarioController@editarUsuario')->name('editarUsuario');
     Route::post('/usuarios/atualizar/{id}', 'UsuarioController@atualizarUsuario')->name('atualizarUsuario');
-    Route::get('/usuarios/visualizar/{id}', 'UsuarioController@visualizarUsuario')->name('visualizarUsuario');
     Route::get('/usuarios/excluir/{id}', 'UsuarioController@excluirUsuario')->name('excluirUsuario');
     Route::get('/usuarios/cadastrar', 'UsuarioController@cadastrarUsuario')->name('cadastrarUsuario');
     Route::post('/usuarios/salvar', 'UsuarioController@salvarUsuario')->name('salvarUsuario');
@@ -35,24 +33,19 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/pessoas/fornecedores/cadastrar', 'FornecedorController@cadastrarFornecedor')->name('cadastrarFornecedor');
     Route::post('/pessoas/fornecedores/salvar', 'FornecedorController@salvarFornecedor')->name('salvarFornecedor');
     Route::get('/pessoas/fornecedores/excluir/{id}', 'FornecedorController@excluirFornecedor')->name('excluirFornecedor');
-    Route::get('/pessoas/fornecedores/editar/{id}', 'FornecedorController@editarFornecedor')->name('editarFornecedor');
     Route::post('/pessoas/fornecedores/atualizar/{id}', 'FornecedorController@atualizarFornecedor')->name('atualizarFornecedor');
 
     /* Rotas Protegidas de Produtos */
     Route::get('/produtos', 'ProdutoController@listarProdutos')->name('listarProdutos');
     Route::get('/produtos/excluir/{id}', 'ProdutoController@excluirProduto')->name('excluirProduto');
-    Route::get('/produtos/editar/{id}', 'ProdutoController@editarProduto')->name('editarProduto');
     Route::post('/produtos/atualizar/{id}', 'ProdutoController@atualizarProduto')->name('atualizarProduto');
-    Route::get('/produtos/visualizar/{id}', 'ProdutoController@visualizarProduto')->name('visualizarProduto');
     Route::post('/produtos/salvar', 'ProdutoController@salvarProduto')->name('salvarProduto');
     Route::get('/produtos/cadastrar', 'ProdutoController@cadastrarProduto')->name('cadastrarProduto');
 
     /* Rotas Protegidas de Setores */
     Route::get('/produtos/setores', 'SetorController@listarSetores')->name('listarSetores');
     Route::get('/produtos/setores/excluir/{id}', 'SetorController@excluirSetor')->name('excluirSetor');
-    Route::get('/produtos/setores/editar/{id}', 'SetorController@editarSetor')->name('editarSetor');
     Route::post('/produtos/setores/atualizar/{id}', 'SetorController@atualizarSetor')->name('atualizarSetor');
-    Route::get('/produtos/setores/visualizar/{id}', 'SetorController@visualizarSetor')->name('visualizarSetor');
     Route::get('/produtos/setores/cadastrar', 'SetorController@cadastrarSetor')->name('cadastrarSetor');  
     Route::post('/produtos/setores/salvar', 'SetorController@salvarSetor')->name('salvarSetor');
 
@@ -60,9 +53,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/produtos/categorias', 'CategoriaController@listarCategorias')->name('listarCategorias');
     Route::get('/produtos/categorias/cadastrar', 'CategoriaController@cadastrarCategoria')->name('cadastrarCategoria');      
     Route::post('/produtos/categorias/salvar', 'CategoriaController@salvarCategoria')->name('salvarCategoria');
-    Route::get('/produtos/categorias/editar/{id}', 'CategoriaController@editarCategoria')->name('editarCategoria');
     Route::post('/produtos/categorias/atualizar/{id}', 'CategoriaController@atualizarCategoria')->name('atualizarCategoria');
-    Route::get('/produtos/categorias/visualizar/{id}', 'CategoriaController@visualizarCategoria')->name('visualizarCategoria');
     Route::get('/produtos/categorias/excluir/{id}', 'CategoriaController@excluirCategoria')->name('excluirCategoria');
 
     /* Rotas Protegidas de Sub-Categorias */
@@ -70,35 +61,27 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/produtos/categorias/subcategorias/cadastrar', 'SubcategoriaController@cadastrarSubcategoria')->name('cadastrarSubcategoria');  
     Route::post('/produtos/categorias/subcategorias/salvar', 'SubcategoriaController@salvarSubcategoria')->name('salvarSubcategoria');  
     Route::get('/produtos/categorias/subcategorias/excluir/{id}', 'SubcategoriaController@excluirSubcategoria')->name('excluirSubcategoria');
-    Route::get('/produtos/categorias/subcategorias/editar/{id}', 'SubcategoriaController@editarSubcategoria')->name('editarSubcategoria');
     Route::post('/produtos/categorias/subcategorias/atualizar/{id}', 'SubcategoriaController@atualizarSubcategoria')->name('atualizarSubcategoria');
-    Route::get('/produtos/categorias/subcategorias/visualizar/{id}', 'SubcategoriaController@visualizarSubcategoria')->name('visualizarSubcategoria');
 
     /* Rotas Protegidas de Unidades */
     Route::get('/produtos/unidades', 'UnidadeController@listarUnidades')->name('listarUnidades');
     Route::get('/produtos/unidades/cadastrar', 'UnidadeController@cadastrarUnidade')->name('cadastrarUnidade');  
     Route::post('/produtos/unidades/salvar', 'UnidadeController@salvarUnidade')->name('salvarUnidade');    
-    Route::get('/produtos/unidades/editar/{id}', 'UnidadeController@editarUnidade')->name('editarUnidade');
     Route::post('/produtos/unidades/atualizar/{id}', 'UnidadeController@atualizarUnidade')->name('atualizarUnidade');
-    Route::get('/produtos/unidades/visualizar/{id}', 'UnidadeController@visualizarUnidade')->name('visualizarUnidade');
     Route::get('/produtos/unidades/excluir/{id}', 'UnidadeController@excluirUnidade')->name('excluirUnidade');
 
     /* Rotas Protegidas de Marcas */
     Route::get('/produtos/marcas', 'MarcaController@listarMarcas')->name('listarMarcas');
     Route::get('/produtos/marcas/cadastrar', 'MarcaController@cadastrarMarca')->name('cadastrarMarca');  
     Route::post('/produtos/marcas/salvar', 'MarcaController@salvarMarca')->name('salvarMarca');  
-    Route::get('/produtos/marcas/editar/{id}', 'MarcaController@editarMarca')->name('editarMarca');  
     Route::post('/produtos/marcas/atualizar/{id}', 'MarcaController@atualizarMarca')->name('atualizarMarca');
-    Route::get('/produtos/marcas/visualizar/{id}', 'MarcaController@visualizarMarca')->name('visualizarMarca');
     Route::get('/produtos/marcas/excluir/{id}', 'MarcaController@excluirMarca')->name('excluirMarca');
 
     /* Rotas Protegidas de Cargos */
      Route::get('/usuarios/cargos', 'CargoController@listarCargos')->name('listarCargos');
     Route::get('/usuarios/cargos/cadastrar', 'CargoController@cadastrarCargo')->name('cadastrarCargo');  
     Route::post('/usuarios/cargos/salvar', 'CargoController@salvarCargo')->name('salvarCargo');  
-    Route::get('/usuarios/cargos/editar/{id}', 'CargoController@editarCargo')->name('editarCargo');  
     Route::post('/usuarios/cargos/atualizar/{id}', 'CargoController@atualizarCargo')->name('atualizarCargo');
-    Route::get('/usuarios/cargos/visualizar/{id}', 'CargoController@visualizarCargo')->name('visualizarCargo');
     Route::get('/usuarios/cargos/excluir/{id}', 'CargoController@excluirCargo')->name('excluirCargo');
 
   /* Rotas Protegidas de Formas de Pagamentos */
@@ -106,9 +89,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/financeiro/formaspagamentos/cadastrar', 'FormaPagamentoController@cadastrarFormaPag')->name('cadastrarFormaPag');  
     Route::post('/financeiro/formaspagamentos/salvar', 'FormaPagamentoController@salvarFormaPag')->name('salvarFormaPag');  
     Route::get('/financeiro/formaspagamentos/excluir/{id}', 'FormaPagamentoController@excluirFormaPag')->name('excluirFormaPag');
-    Route::get('/financeiro/formaspagamentos/visualizar/{id}', 'FormaPagamentoController@visualizarFormaPag')->name('visualizarFormaPag');
-    Route::post('/financeiro/formaspagamentos/atualizar/{id}', 'FormaPagamentoController@atualizarFormaPag')->name('atualizarFormaPag');
-    Route::get('/financeiro/formaspagamentos/editar/{id}', 'FormaPagamentoController@editarFormaPag')->name('editarFormaPag');  
+    Route::post('/financeiro/formaspagamentos/atualizar/{id}', 'FormaPagamentoController@atualizarFormaPag')->name('atualizarFormaPag'); 
 
     /* Rotas Utilizadas no Ajax */
     Route::get('ajax/pegar-lista-categorias','ProdutoController@getCategoriasAjax');
