15777f1ae1ffdf40266c6e131eb591fd7efac912
Kelvin Ferreira Souza <kelvin@ferreirasouza.com>
2018-06-25 21:06:32 -0300

    Heroku

--- database/seeds/UsuarioTableSeeder.php
+++ database/seeds/UsuarioTableSeeder.php
@@ -11,16 +11,6 @@ class UsuarioTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('usuarios')->insert([
-            'nome'          => 'Administrador',
-            'login'         => 'admin',
-            'email'         => 'admin@admin.com',
-            'senha'         =>  bcrypt('admin'),
-            'cpf'           =>  '000.000.000-73',
-            'rg'            =>  '0000000000',
-            'cargoId'       =>  1,
-            'sexo'          =>  'Masculino',
-            'dataNasc'      =>  '1997-01-01'
-        ]);
+        
     }
 }
