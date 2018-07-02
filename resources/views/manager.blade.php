789ecb588f4b87c07e1dddc4ebc24f453cc9619d
Kelvin Ferreira Souza <kelvin@ferreirasouza.com>
2018-07-02 20:10:27 -0300

    Adicionado Widgets ao manager

--- resources/views/manager.blade.php
+++ resources/views/manager.blade.php
@@ -22,5 +22,57 @@
                         </ul>
                     </div>
                 </div>
+            </div>
+            <div class="container bootstrap snippet" style="margin-top: -2%; max-width: 1500px">
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="circle-tile ">
+                            <a href="#"><div class="circle-tile-heading dark-blue"><i class="fa fa-users fa-fw fa-3x"></i></div></a>
+                            <div class="circle-tile-content dark-blue">
+                                <div class="circle-tile-description text-faded"> Clientes </div>
+                                <div class="circle-tile-number text-faded ">265</div>
+                                <a class="circle-tile-footer" href="#">Detalhes <i class="fa fa-chevron-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
 
-                @endsection
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="circle-tile ">
+                            <a href="#"><div class="circle-tile-heading red"><i class="fa fa-truck fa-3x"></i></div></a>
+                            <div class="circle-tile-content red">
+                                <div class="circle-tile-description text-faded"> Pedidos </div>
+                                <div class="circle-tile-number text-faded ">10</div>
+                                <a class="circle-tile-footer" href="#">Detalhes <i class="fa fa-chevron-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div> 
+
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="circle-tile ">
+                            <a href="#"><div class="circle-tile-heading green"><i class="fa fa-money fa-fw fa-3x"></i></div></a>
+                            <div class="circle-tile-content green">
+                                <div class="circle-tile-description text-faded"> Vendas do Dia </div>
+                                <div class="circle-tile-number text-faded ">R$ 529,90</div>
+                                <a class="circle-tile-footer" href="#">Detalhes <i class="fa fa-chevron-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="circle-tile ">
+                            <a href="#"><div class="circle-tile-heading purple"><i class="fa fa-line-chart fa-fw fa-3x"></i></div></a>
+                            <div class="circle-tile-content purple">
+                                <div class="circle-tile-description text-faded"> Users Online </div>
+                                <div class="circle-tile-number text-faded ">10</div>
+                                <a class="circle-tile-footer" href="#">Detalhes <i class="fa fa-chevron-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+@endsection
