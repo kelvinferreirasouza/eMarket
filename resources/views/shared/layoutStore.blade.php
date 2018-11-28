287af7403a3bf70ac17b3c50be5fbd0b052d0ca6
Kelvin Ferreira Souza <kelvin@ferreirasouza.com>
2018-11-28 01:06:29 -0200

    Ajustes na responsividade da pagina

--- resources/views/shared/layoutStore.blade.php
+++ resources/views/shared/layoutStore.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
         <script src="{{ asset('js/store/jquery-3.3.1.js') }}"></script>
         <link href="{{ asset('css/store/bootstrap.min.css') }}" rel="stylesheet" id="bootstrap-css">
         <script src="{{ asset('js/store/bootstrap.min.js') }}"></script>
         <script src="{{ asset('js/store/store.js') }}" type="text/javascript"></script>
         <link rel="stylesheet" href="{{ asset('css/store/store.css') }}" type="text/css"/>
+        <link rel="stylesheet" href="{{ asset('css/store/mobile.css') }}" type="text/css"/>
         <!-- Font Awesome -->
         <link rel="stylesheet" href="{{url('assets/css/all.css')}}">
         <link rel="shortcut icon" type="image/ico" href="../../imgs/favicon.ico"/>
@@ -52,8 +54,8 @@
                 </div>
                 <div class="row row2">
                     <div class="col-sm-2">
-                        <h2 style="margin:0px;"><span class="smallnav menu" onclick="openNav()">☰ Menu</span></h2>
-                        <h1 style="margin:0px;"><span class="largenav"><a href="{{ route('index') }}"><img class="logoNav" src="{{ asset('../imgs/logoContorno2.png') }}"></a></span></h1>
+                        <h2 style="margin:0px;"><span class="smallnav menu" onclick="openNav()">☰ </span></h2>
+                        <h1 style="margin:0px;"><span class=""><a href="{{ route('index') }}"><img class="logoNav" src="{{ asset('../imgs/logoContorno2.png') }}"></a></span></h1>
                     </div>  
                     <div class="navbar-search smallsearch col-sm-8 col-xs-11">
                         <div class="row">
@@ -69,7 +71,7 @@
                             {!! Form::close() !!}
                         </div>
                     </div>
-                    <div class="cart largenav col-sm-2">
+                    <div class="cart col-sm-2">
                         <a href="{{ route('carrinho') }}" class="cart-button">
                             <svg class="cart-svg " width="16 " height="16 " viewBox="0 0 16 16 ">
                             <path d="M15.32 2.405H4.887C3 2.405 2.46.805 2.46.805L2.257.21C2.208.085 2.083 0 1.946 0H.336C.1 0-.064.24.024.46l.644 1.945L3.11 9.767c.047.137.175.23.32.23h8.418l-.493 1.958H3.768l.002.003c-.017 0-.033-.003-.05-.003-1.06 0-1.92.86-1.92 1.92s.86 1.92 1.92 1.92c.99 0 1.805-.75 1.91-1.712l5.55.076c.12.922.91 1.636 1.867 1.636 1.04 0 1.885-.844 1.885-1.885 0-.866-.584-1.593-1.38-1.814l2.423-8.832c.12-.433-.206-.86-.655-.86 " fill="#fff "></path>
@@ -95,6 +97,10 @@
             <a href="#">Link</a>
             <a href="#">Link</a>
             <a href="#">Link</a>
+            <a href="{{route('minhaConta')}}"><i class="far fa-address-card"></i> Minha Conta</a>
+            <a class="profile-links" href="{{route('meusPedidos')}}"><i class="fas fa-dolly"></i> Meus Pedidos</a>
+
+
         </div>
 
         <div class="subnav navbar-default">
